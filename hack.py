36e08a4d7820b8844d4771f2f4d326d20f6cb720
Daniel Tse <972789+tsetsefly@users.noreply.github.com>
2024-12-25 00:47:24 -0800

    adding in ability to handle c-instructions and output binary representation

--- hack.py
+++ hack.py
@@ -1,11 +1,10 @@
 import os
 import sys
 
-def create_binary_string(a_number):
+def create_binary_number_string(a_number):
 	no_pad_binary = bin(a_number)[2:]
 	pad_binary = no_pad_binary.zfill(16)
 	return pad_binary
-
 
 
 def parse_c_instruction(instruction):
@@ -34,15 +33,106 @@ def parse_c_instruction(instruction):
         "jump": jump
     }
 
+
+def find_comp_type(comp, comp_0_table, comp_1_table):
+	if comp in comp_0_table:
+		return "0"
+
+	if comp in comp_1_table:
+		return "1"
+
+	return None
+
+
+def create_binary_c_instruct(parts):
+	a_value = "0"
+
+	comp_0_table = {
+		"0":	"101010",
+		"1":	"111111",
+		"-1":	"111010",
+		"D":	"001100",
+		"A":	"110000",
+		"!D":	"001101",
+		"!A":	"110001",
+		"-D":	"001111",
+		"-A":	"110011",
+		"D+1":	"011111",
+		"A+1":	"110111",
+		"D-1":	"001110",
+		"A-1":	"110010",
+		"D+A":	"000010",
+		"D-A":	"010011",
+		"A-D":	"000111",
+		"D&A":	"000000",
+		"D|A":	"010101"
+	}
+	comp_1_table = {
+		"M":	"110000",
+		"!M":	"110001",
+		"-M":	"110011",
+		"M+1":	"110111",
+		"M-1":	"110010",
+		"D+M":	"000010",
+		"D-M":	"010011",
+		"M-D":	"000111",
+		"D&M":	"000000",
+		"D|M":	"010101"
+	}
+	dest_table = {
+		"null":	"000",
+		"M":	"001",
+		"D":	"010",
+		"DM":	"011",
+		"A":	"100",
+		"AM":	"101",
+		"AD":	"110",
+		"ADM":	"111"
+	}
+	jump_table = {
+		"null":	"000",
+		"JGT":	"001",
+		"JEQ":	"010",
+		"JGE":	"011",
+		"JLT":	"100",
+		"JNE":	"101",
+		"JLE":	"110",
+		"JMP":	"111"
+	}
+
+	# finding destination binary value in table
+	dest = dest_table.get(parts['dest'], "000")
+
+	# finding comparison binary value in table
+	comp_type = find_comp_type(parts['comp'], comp_0_table, comp_1_table)
+	if comp_type == "0":
+		comp = comp_0_table.get(parts['comp'], None)
+	elif comp_type == "1":
+		comp = comp_1_table.get(parts['comp'], None)
+		a_value = "1"
+	
+	# finding jump binary value in table
+	if parts['jump']:
+		jump = jump_table.get(parts['jump'], "000")
+	else:
+		jump = "000"
+
+	c_instruct_binary = "111" + a_value + comp + dest + jump
+
+	return c_instruct_binary
+
+
 def main():
     a_number_string = "7" # test code
     a_number = int(a_number_string)
-    a_instruct = create_binary_string(a_number)
+    a_instruct = create_binary_number_string(a_number)
     # print(a_instruct) # test code
     # print(type(a_instruct)) # test code
 
-    c_instruct_string = "AM=M+1"
+    c_instruct_string = "A=-1"
     c_instruct_parts = parse_c_instruction(c_instruct_string)
+    c_instruct = create_binary_c_instruct(c_instruct_parts)
+    print(c_instruct)
     # print(f"\nInstruction: {c_instruct_string}") # test code
     # print(f"Destination: {c_instruct_parts['dest']}") # test code
     # print(f"Computation: {c_instruct_parts['comp']}") # test code
